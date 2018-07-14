ab5a7ebf747c23a5d61733adef1f6fc51dfb8f17
wangyongtao <wangyt4dev@gmail.com>
2018-07-14 14:25:24 +0800

    Config Server集成到注册中心

--- eureka-server/src/main/java/github/wyt/EurekaServerApplication.java
+++ eureka-server/src/main/java/github/wyt/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 注册中心 http://localhost:8761/
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
